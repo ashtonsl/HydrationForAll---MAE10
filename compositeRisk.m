7a137b2987d564fb5e81c03e7079f16ecb16bb6f
faromer1 <faromer1@uci.edu>
2026-03-02 18:43:21 -0800

    Update compositeRisk.m

--- compositeRisk.m
+++ compositeRisk.m
@@ -1,8 +1,10 @@
 function score = compositeRisk(nitrate, arsenic, lead, chlorine, fluoride, pfas, ...
+                                pce, benzene, radon, ...
                                 nitrate_lim, arsenic_lim, lead_lim, ...
-                                chlorine_lim, fluoride_lim, pfas_lim)
-% compositeRisk - Returns a 0-10 risk score for one city
-% Each of 5 contaminants contributes 0 (OK), 1 (Borderline), or 2 (High Concern)
+                                chlorine_lim, fluoride_lim, pfas_lim, ...
+                                pce_lim, benzene_lim, radon_lim)
+% compositeRisk - Returns a 0-18 risk score for one city
+% Each of 9 contaminants contributes 0 (OK), 1 (Borderline), or 2 (High Concern)
 
 [~, s1] = qualityRating(nitrate,   nitrate_lim);
 [~, s2] = qualityRating(arsenic,   arsenic_lim);
@@ -10,6 +12,10 @@ function score = compositeRisk(nitrate, arsenic, lead, chlorine, fluoride, pfas,
 [~, s4] = qualityRating(chlorine,  chlorine_lim);
 [~, s5] = qualityRating(fluoride,  fluoride_lim);
 [~, s6] = qualityRating(pfas,      pfas_lim);
-score = s1 + s2 + s3 + s4 + s5 + s6;
+[~, s7] = qualityRating(pce,       pce_lim);
+[~, s8] = qualityRating(benzene,   benzene_lim);
+[~, s9] = qualityRating(radon,     radon_lim);
+score = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9;
 end
+
 
